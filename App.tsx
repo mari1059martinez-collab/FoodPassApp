6c5f196dd24473d29f3cf047628a92d26c06207f
Lisbeth-2007 <lisbethdorado2007@gmail.com>
2026-07-01 18:15:11 -0500

    Mi primera versión de la página de puntos

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.tsx
+
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import PointsScreen from './src/screens/PointsScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <PointsScreen />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
